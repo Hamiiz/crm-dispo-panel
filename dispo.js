e3ec3f2667d0447c55d76e2205b0eb847f7b2a6f
Hamza Mohammed <hmmhsd37@gmail.com>
2025-11-08 12:54:23 +0300

    Update match pattern in dispo.js to restrict URL matching

--- dispo.js
+++ dispo.js
@@ -4,7 +4,7 @@
 // @version      2025-11-06
 // @description  try to take over the world!
 // @author       You
-// @match        *://*
+// @match        *://
 // @grant        none
 // ==/UserScript==
 
